2ede27eb022c32cc43775dacd0e4ee9693ba6133
MatejGardos <313matej@gmail.com>
2023-08-09 13:13:26 +0200

    Fixed button collisions

--- moduls/buttons.py
+++ moduls/buttons.py
@@ -9,11 +9,13 @@ class Button():
         self.txt = txt
         self.button_color = cfg.BUTTON_COLOR
         self.font = pg.font.Font("assets/Font/Franxurter.ttf", 50)
-        
+
+
+        self.button_rect = pg.rect.Rect((self.x, self.y, 150,70))
         
         self.text = self.font.render(txt, True, cfg.BLACK)
         self.rect = self.text.get_rect()
-        self.rect.center = (x+75,y+35)
+        self.rect.center = (self.x+75,self.y+35)
         self.surface.blit(self.text, self.rect)
 
 
@@ -21,7 +23,7 @@ class Button():
     def update(self, button_pressed):
         """change color of button if mouse is hovering and check if it is pressed"""
         mouse_pos = pg.mouse.get_pos()
-        if self.rect.collidepoint(mouse_pos):
+        if self.button_rect.collidepoint(mouse_pos):
             self.button_color = (90, 90, 90)
             if button_pressed:
                 return True
@@ -31,6 +33,6 @@ class Button():
 
 
     def draw(self):
-        pg.draw.rect(self.surface, self.button_color, (self.x, self.y, 150,70))
+        pg.draw.rect(self.surface, self.button_color, self.button_rect)
         pg.draw.rect(self.surface, cfg.BLACK, (self.x, self.y, 150,70), 2)
         self.surface.blit(self.text, self.rect)
